8e391912e440541d59d67582b4074f163d2f4ea1
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-05-19 09:34:11 +0300

    fixed errors using eslint

--- __test__/notes.test.js
+++ __test__/notes.test.js
@@ -5,13 +5,13 @@ const notes = new Notes();
 jest.spyOn(global.console, 'log');
 describe('node Mdule', ()=> {
 
-    it(' does nothing with invalid opts', ()=> {
-      notes.add();
-      expect(console.log).not.toHaveBeenCalled();
-  })
+  it(' does nothing with invalid opts', ()=> {
+    notes.add();
+    expect(console.log).not.toHaveBeenCalled();
+  });
   
   it('will log out opts when given', ()=> {
-      notes.add({url: 'foo'});
-      expect(console.log).toHaveBeenCalled();
-  })        
+    notes.add({url: 'foo'});
+    expect(console.log).toHaveBeenCalled();
+  });        
 });
